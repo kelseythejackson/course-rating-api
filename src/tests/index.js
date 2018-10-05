b813e86ad59dc5a9a1dc8f4aa4927d0071dc30a6
Kelsey Jackson <kelseythej@gmail.com>
2018-10-05 14:12:51 -0500

    Add another test

--- src/tests/index.js
+++ src/tests/index.js
@@ -13,7 +13,7 @@ const assert = require('assert');
 
 chai.use(chaiHttp);
 
-describe('/GET users', () => {
+describe('GET /users', () => {
     let user = new User({
         "fullName": "Kelsey Jackson",
         "emailAddress": "kelsey@jackson.com",
@@ -21,19 +21,8 @@ describe('/GET users', () => {
         "confirmPassword": "kelseyjackson"
 
     });
-    let course = new Course({
-        "title": "Fishscale",
-        "description": "RAGU",
-        "user": {
-            "_id": "5bb4eb2672755561a6a97579"
-        },
-        "steps": [{
-            "title": "Starting off",
-            "description": "Blue and Cream"
-        }]
-    });
+    
     it('should return the authenticated user', (done) => {
-
 
         chai.request(server)
             .get('/api/users')
@@ -48,6 +37,36 @@ describe('/GET users', () => {
                 done();
             });
     })
+
+    it('should return a 401 error because the credentials are invalid', (done) => {
+        chai.request(server)
+            .get('/api/users')
+            .auth('not@toney.com', 'notAValidPass')
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+                expect(res).to.have.status(401);
+
+
+                done();
+            })
+    });
+});
+
+describe('GET /api/courses/:courseId', () => {
+    let course = new Course({
+        "title": "Fishscale",
+        "description": "RAGU",
+        "user": {
+            "_id": "5bb4eb2672755561a6a97579"
+        },
+        "steps": [{
+            "title": "Starting off",
+            "description": "Blue and Cream"
+        }]
+    });
+    console.log(course);
 
     it('should return a 401 error because the credentials are invalid', (done) => {
         chai.request(server)
