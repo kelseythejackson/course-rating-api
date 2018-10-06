da28aa156cadaa71942658aede4bde5dea2dd310
Kelsey Jackson <kelseythej@gmail.com>
2018-10-06 07:01:43 -0500

    added error to get route if user is not logged in

--- src/routes/users.js
+++ src/routes/users.js
@@ -4,16 +4,26 @@ const router = express.Router();
 const User = require('../models').User;
 const mid = require('../middleware');
 
+// Returns the currently authenticated user
 router.get('/', mid.authorize, (req, res, next) => {
   const creds = auth(req);
-  if(creds.name === req.doc.emailAddress) {
-    res.send(req.doc);
+
+  // Checks if 'cred' exists
+  if (creds) {
+    if (creds.name === req.doc.emailAddress) {
+      res.send(req.doc);
+    }
   }
+
+  let err = new Error('You\'e not currently logged in, so you can\'t view this user');
+  err.status = 401;
+  next(err);
+
 });
 
-
+// Creates a new user
 router.post('/', (req, res, next) => {
-  let user = new User(req.body); 
+  let user = new User(req.body);
   user.save((err, user) => {
     if (err) {
       err.status = 400;
